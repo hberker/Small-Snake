a51fc332fcb608fc57cfda50d793a426c095eb65
hberker <hosni.berker@gmail.com>
2019-04-17 08:47:26 -0400

    adds new changes to movement and block spawns

--- main.cpp
+++ main.cpp
@@ -13,73 +13,93 @@ struct snake
     snake(i a) : W(a){}
     i W;
     SDL_Rect rc = {25,25,10,10};
-    vector<vector<i>> b = {{25,25}};
+    vector<vector<i>> b={{25,25}};
        
-    void move(i dir[2], i SH, i SW, bool a)
+    i move(i dir[2], i SH, i SW, bool a)
     {
         if (a) b.push_back({0,0});
-        //if (((dir[0] + b.at(0).at(0)) % 5 == 0) || ((dir[1] + b.at(0).at(1)) % 11 == 0) || true)
-       // {
         for (i e = b.size() - 1; e > 0; e--)b.at(e) = b.at(e - 1);
         if (b.at(0).at(0) + dir[0] > 5 && b.at(0).at(0) + dir[0] < SH - W) b.at(0).at(0) += dir[0];
         if (b.at(0).at(1) + dir[1] > 5 && b.at(0).at(1) + dir[1] < SW - W) b.at(0).at(1) += dir[1];
-        //}
     }
-    void render(SDL_Renderer *rend)
+    bool die()
     {
-        SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0xFF, 0xFF );
+        for(i e = 0; e < b.size();e++)
+        {
+            for(i a = 0; a < b.size();a++)
+            {
+                if(a!=e&&b.at(e).at(0)==b.at(a).at(0)&&b.at(e).at(1)==b.at(a).at(1)) return false;
+            }
+        }
+        return true;
+    }
+    bool didEat(i x, i y)
+    {
+        for(i e = 0; e < b.size();e++)
+        {
+            if(b.at(e).at(0) == x && b.at(e).at(1) == y) return true;
+        }
+        return false;
+    }
+    i rend(SDL_Renderer *rend)
+    {
+        SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0xFF, 0xFF);
         for(i e = 0; e < b.size(); e++)
         {
-            rc.x = b.at(e).at(0), rc.y = b.at(e).at(1) ;
+            rc.x = b.at(e).at(0), rc.y = b.at(e).at(1);
+            SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0xFF, 0xFF);
             SDL_RenderFillRect(rend,&rc);
         }
     }
-
 }; 
+
 int main(int argc, char* argv[])
 {
+
     i s = 10;
     bool a = true;
-    bool eat = true;
+    bool eat = false;
     i SW = 480, SH = 640;
     i Dir[2] = {0,0};
 
     SDL_Window *wind;
     SDL_Renderer *rend;
+    SDL_Rect f = {465,465,10,10};
 
     snake sn = snake(++s);
 
-    wind = SDL_CreateWindow("An SDL2 window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SH,SW,SDL_WINDOW_OPENGL);
-    rend = SDL_CreateRenderer(wind, -1, SDL_RENDERER_ACCELERATED );
+    wind=SDL_CreateWindow("S",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SH,SW,SDL_WINDOW_OPENGL);
+    rend=SDL_CreateRenderer(wind, -1, SDL_RENDERER_ACCELERATED );
     while(a)
     {
+        if(eat)f.x =  25 + (11 * (rand()%(54 + 1))), f.y = 25 + (11 * (rand()%(40 + 1))),eat = false;
+
         SDL_Event e;
-        
-        //Handle events on queue 
-        while( SDL_PollEvent( &e ) != 0 ) 
+        while( SDL_PollEvent(&e)!= 0) 
         { 
-            if( e.type == SDL_QUIT ) a = false;  
-            if( e.type == SDL_KEYDOWN ) 
+            if(e.type==SDL_QUIT)a=!a;  
+            if(e.type==SDL_KEYDOWN) 
             { 
-                switch( e.key.keysym.sym ) 
+                switch(e.key.keysym.sym) 
                 { 
-                    case SDLK_UP: Dir[0] = 0, Dir[1] = -s; break; 
-                    case SDLK_DOWN:  Dir[0] = 0, Dir[1] = s; break; 
-                    case SDLK_LEFT:  Dir[0] = -s, Dir[1] = 0; break; 
-                    case SDLK_RIGHT:  Dir[0] = s, Dir[1] = 0; break; 
-                    default:  Dir[0] = 0, Dir[1] = 0; break; 
+                    case SDLK_UP: Dir[0]=0,Dir[1]=-s;break; 
+                    case SDLK_DOWN:Dir[0]=0,Dir[1]=s;break; 
+                    case SDLK_LEFT:Dir[0]=-s,Dir[1]=0;break; 
+                    case SDLK_RIGHT:Dir[0]=s,Dir[1]=0;break; 
+                    default:Dir[0]=0,Dir[1]=0;break; 
                 } 
             } 
         }
-
         SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0x00 );
         SDL_RenderClear(rend);
+        eat = sn.didEat(f.x,f.y);
         sn.move(Dir, SH, SW, eat);
-        sn.render(rend);
+        sn.rend(rend);
+        SDL_RenderFillRect(rend,&f);
         SDL_RenderPresent(rend);
         SDL_Delay(40);
+        a=sn.die();
     }
     SDL_Quit();
     return 0;
-
 }
